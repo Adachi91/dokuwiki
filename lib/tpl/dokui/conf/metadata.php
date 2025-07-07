06608bacdc11405a98e132809385862b1a7ae586
Adachi Sakura <adachi9119@gmail.com>
2025-07-07 18:43:21 +0000

    adsf

--- lib/tpl/dokui/conf/metadata.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$meta['width'] = array('string');
-$meta['font-size'] = array('string');
-$meta['title-color'] = array('string');
-$meta['hide-entry-title'] = array('onoff');
-$meta['footer-text'] = array('string');
